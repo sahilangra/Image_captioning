5dd704b4cd20eb7e17dcb7eecc95aca0f6e67a2f
Sahil Angra <sahilangra@gmail.com>
2021-02-04 23:36:07 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        
+        self.embed_layer = nn.Embedding(vocab_size, embed_size)
+        self.lstm_layer = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear_layer = nn.Linear(hidden_size, vocab_size)
+    
+    def forward(self, features, captions):
+        embeddings = self.embed_layer(captions[:,:-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm_layer(embeddings)
+        outputs = self.linear_layer(hiddens)
+        
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        sampled_ids = []
+
+        for i in range(20):
+            hiddens, states = self.lstm_layer(inputs, states)
+            outputs = self.linear_layer(hiddens.squeeze(1))
+            predicted = outputs.max(1)[1]
+            sampled_ids.append(predicted.tolist()[0])
+            inputs = self.embed_layer(predicted)
+            inputs = inputs.unsqueeze(1)
+            
+            if((predicted.tolist()[0])==1):
+                break;
+                
+        return sampled_ids
